e0be4a1adbbd998917020f29d2732a6228d81327
root <mhn3@sfu.ca>
2019-10-04 00:37:14 +0000

    set DATABASEURL env for local and on heroku

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ const commentRoutes 		= require("./routes/comments"),
 	authRoutes			= require("./routes/index");
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
-// mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true });
-mongoose.connect("mongodb+srv://mhn3:5991119qwerty@cluster0-ervpx.mongodb.net/yelp_camp?retryWrites=true&w=majority",{ useNewUrlParser: true });
+
+mongoose.connect(process.env.DATABASEURL,{ useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+"/public"));
 
